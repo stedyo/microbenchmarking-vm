2973a1e36f224af64a4bb725f30941b05f0ba559
stephanyzd <stephanydionysio@gmail.com>
2016-06-08 01:04:42 -0300

    gnuplot scripts

--- GNUPLOT_SCRIPTS/E1/E1_QoS_Variation.sh
+++ GNUPLOT_SCRIPTS/E1/E1_QoS_Variation.sh
@@ -1,9 +1,11 @@
 #!/usr/local/bin/gnuplot
 
-set term canvas enhanced font 'Helvetica,10'
-#set title "Variação de Taxa de Transmissão e Utilização de CPU"
+#set term canvas enhanced font 'Helvetica,10'
 
-set rmargin 5
+set terminal png
+set output "e1_latency_variation.png"
+
+
 set style data histogram
 set style histogram cluster gap 1
 set size square
@@ -17,19 +19,18 @@ set style line 2 lc rgb "black" lt 2 dashtype 3
 
 
 
-
-set yrange [100:120]
-set ytics "5" nomirror
-set y2range [0:100]
+set yrange [0:3]
+set ytics "1" nomirror
+set y2range [20:70]
 set y2tics "10"
 
 set xtics nomirror
-set xlabel 'Tamanho dos Pacotes de Rede' 
-set ylabel 'Taxa de Transmissao (MB/S)'  rotate by 90
+set xlabel 'Qtde. Dominios no Sistema Hospedeiro' 
+set ylabel 'Latencia Media (ms)'  rotate by 90
 set y2label 'Uso CPU (%)'  rotate by 90
 
 set auto x
-plot 'E0_Workload_Definition_Multaxis_Histogram_DATA.dat' using 2:xtic(1) title col axes x1y1 with linespoints ls 1, \
+plot 'e1_qos_variation.dat' using 2:xtic(1) title col axes x1y1 with linespoints ls 1, \
         '' using 3:xtic(1) title col axes x1y2 with linespoints ls 2 
         
         
--- GNUPLOT_SCRIPTS/simple_line_graph.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/gnuplot
-
-set term latex
-set ytics "100"
-set yrange [21500:23000]
-
-plot "test_line.dat" w points
-
-
